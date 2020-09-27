62cfd6a3d8bfc6ac1e5412dcf44b12fa3b1573f6
Siwakorn Punyawatthananukool <70639316+SiwakornP@users.noreply.github.com>
2020-09-27 23:41:43 +0900

    Update cachematrix.R
    Add my code for assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
 ## Put comments here that give an overall description of what your
-## functions do
+## functions to compute inverse matrix and cache those matrix
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+#This function create a special object, which has 4 different functions to interact with "cacheSolve" function
+makeCacheMatrix <- function(mat = matrix() ){
+        invs <- NULL
+        set <- function(new.mat){
+                mat <<- new.mat
+                invs <<- NULL
+        }
+        get <- function() mat
+        setinv <- function(cal.invs) invs <<- cal.invs 
+        getinv <- function() invs
+        list(set = set, get = get, getinv = getinv, setinv = setinv)
 }
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+#This function take special object from makeCacheMatrix to compute the inverse matrix
+cacheSolve <- function(S.mat){
+        if(!is.null(S.mat$getinv() )){
+                print("Cache matrix is used...")
+                return(S.mat$getinv() )
+        }
+        print("Calculate inverse matrix...")
+        require(MASS)
+        tmp = S.mat$get()
+        cal.inv <- solve(tmp)
+        print(cal.inv)
+        invs <<- cal.inv
+        S.mat$setinv(invs)
 }
